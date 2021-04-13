278ebce6ce0a30e4ca98142331567c97fe30238b
Mark Koelle <mkoelle@gmail.com>
2021-04-13 16:33:51 -0500

    output those urls

--- deploy-content.sh
+++ deploy-content.sh
@@ -8,10 +8,12 @@ region='us-east-1'
 export AWS_DEFAULT_REGION=$region
 export AWS_REGION=$region
 
-s3_bucket_key='demo-site-content-bucket'
+projectkey='demo-site-content'
 
 set -x
 
-bucket=$(aws cloudformation list-exports --query "Exports[?Name=='${s3_bucket_key}'].Value" --output text)
+bucket=$(aws cloudformation list-exports --query "Exports[?Name=='${projectkey}-bucket'].Value" --output text)
+URL=$(aws cloudformation list-exports --query "Exports[?Name=='${projectkey}-domain'].Value" --output text)
 
 aws s3 sync src s3://${bucket}
+echo "check it out at $URL"
